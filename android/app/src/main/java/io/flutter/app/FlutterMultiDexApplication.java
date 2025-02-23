f31046484bec975cc389e984d3d62b32451a82a1
psotis <psotakos@gmail.com>
2025-02-23 23:16:11 +0200

    integrating all platforms for firebase

--- android/app/src/main/java/io/flutter/app/FlutterMultiDexApplication.java
+++ android/app/src/main/java/io/flutter/app/FlutterMultiDexApplication.java
@@ -7,19 +7,15 @@
 
 package io.flutter.app;
 
-import android.app.Application;
 import android.content.Context;
-import androidx.annotation.CallSuper;
 import androidx.multidex.MultiDex;
+import io.flutter.app.FlutterApplication;
 
-/**
- * Extension of {@link android.app.Application}, adding multidex support.
- */
-public class FlutterMultiDexApplication extends Application {
-  @Override
-  @CallSuper
-  protected void attachBaseContext(Context base) {
-    super.attachBaseContext(base);
-    MultiDex.install(this);
-  }
+public class FlutterMultiDexApplication extends FlutterApplication {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
+
